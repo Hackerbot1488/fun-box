59fef86dbddccb15d50ac64ca5766d7335150601
Hackerbot1488 <gromovartem04@mail.ru>
2020-08-26 15:35:26 +0300

    added cards to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React from "react";
+import { Card } from "./components/Card";
+import { CardProps } from "./components/Card";
+const cards: CardProps[] = [
+	{
+		taste: "с фуа-гра",
+		weight: "0,5",
+		pieces: 10,
+		mouses: 1,
+		moreInfo: "",
+		disabled: false,
+		activeText: "Печень утки разварная с артишоками.",
+	},
+	{
+		taste: "с рыбой",
+		weight: "2",
+		pieces: 40,
+		mouses: 2,
+		moreInfo: "",
+		disabled: false,
+		activeText: "Головы щучьи с чесноком да свежайшая сёмгушка.",
+	},
+	{
+		taste: "с курой",
+		weight: "5",
+		pieces: 100,
+		mouses: 5,
+		moreInfo: "заказчик доволен",
+		disabled: true,
+		activeText: "Филе из цыплят с трюфелями в бульоне.",
+	},
+];
+export const App: React.FC<{}> = () => {
+	return (
+		<>
+			<p className="title">Ты сегодня покормил кота?</p>
+			<div className="container">
+				{cards.map((card: CardProps) => {
+					return <Card {...card} />;
+				})}
+			</div>
+		</>
+	);
+};
